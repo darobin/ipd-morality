19f8c7f7412df186aafabb1cf26e27dcd3985603
Robin Berjon <robin@berjon.com>
2014-07-28 12:44:58 +0200

    ported the arena code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,62 @@
 
+// error messages
+window.error = function (msg) {
+    var $content;
+    if (Array.isArray(msg)) {
+        $content = $("<ul></ul>");
+        msg.forEach(function (m) {
+            $("<li></li>").text(m).appendTo($content);
+        });
+    }
+    else {
+        $content = $(document.createTextNode(msg));
+    }
+    $("#msg-error")
+        .find("span.message").empty().append($content).end()
+        .show()
+    ;
+};
 
 
+/*
+    from Arena
+    if __name__ == "__main__":
+
+        import the_bots
+
+        a = Arena()
+
+        #----------#
+
+        num_meetings = 5
+
+        b1 = the_bots.ALL_D()
+        b2 = the_bots.ALL_C()
+        b3 = the_bots.RANDOM(p_cooperate=0.5)
+        b4 = the_bots.PAVLOV()
+        b5 = the_bots.TIT_FOR_TAT()
+        b6 = the_bots.TIT_FOR_TWO_TATS()
+        b7 = the_bots.TWO_TITS_FOR_TAT()
+        b8 = the_bots.SUSPICIOUS_TIT_FOR_TAT()
+        b9 = the_bots.GENEROUS_TIT_FOR_TAT(p_generous=0.1)
+        b10 = the_bots.GENEROUS_TIT_FOR_TAT(p_generous=0.3)
+        b11 = the_bots.JOSS(p_sneaky=0.1)
+        b12 = the_bots.JOSS(p_sneaky=0.3)
+        b13 = the_bots.MAJORITY(soft=True)
+        b14 = the_bots.MAJORITY(soft=False)
+        b15 = the_bots.TESTER()
+        b16 = the_bots.FRIEDMAN()
+        b17 = the_bots.EATHERLY()
+        b18 = the_bots.CHAMPION()
+        b19 = the_bots.RANDOM(p_cooperate=0.8)
+        b20 = the_bots.RANDOM(p_cooperate=0.2)
+        bot_list = [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13,\
+         b14, b15, b16, b17, b18, b19, b20]
+
+        t = a.runTournament(bot_list, num_meetings)
+        print(t)
+
+        mc = mc.MoralityCalculator(t)
+        print(mc)
+    
+*/
---
+++ js/arena.js
@@ -0,0 +1,142 @@
+/*
+    The arena which runs tournaments of bots
+    Ported from https://github.com/tscizzle/IPD_Morality/
+***************************************************************************************************/
+
+(function (global) {
+    function Arena () {}
+    Arena.prototype = {
+        /*  Based on a probability of continuing each step, generate
+            interaction lengths for the bot pairs
+
+            ARGS:
+            - w: probability of interaction continuing at each step
+            - numMeetings: number of interaction_lengths needed to be
+            generated
+
+            RETURNS:
+            - interaction_lengths: a list of integers representing how
+            long each meeting between bots will be (if the list is n
+            long, it is because each bot pair meets n times)
+        */
+        generateInteractionLengths: function (w, numMeetings) {
+            var interactionLengths = []
+            ,   i = 0
+            ;
+            while (i < numMeetings) {
+                var meetingLength = 1;
+                while (true) {
+                    if (Math.random() > w) break;
+                    meetingLength++;
+                }
+                interactionLengths.push(meetingLength);
+                i++;
+            }
+            return interactionLengths;
+        }
+        /*  Two bots paired together interacting
+
+            ARGS:
+            - bot1, bot2: instances of BotPlayer (presumably subclasses
+            of BotPlayer), representing the two participating bots
+            - interaction_length: how many turns bot1 and bot2 play in
+            this interaction
+
+            RETURNS:
+            - past_moves: list of every move that occurred during the
+            interaction
+        */
+    ,   botInteraction:  function (bot1, bot2, interactionLength, payoffs, w) {
+            // Temptation (tricks the other), Reward (mutual cooperation)
+            // Punishment (mutual defection), Sucker (got tricked)
+            if (!payoffs) payoffs = { T: 5, R: 3, P: 1, S: 0 };
+            if (typeof w === "undefined") w = 0.995;
+            var pastMoves1 = []
+            ,   pastMoves2 = []
+            ,   i = 0
+            ;
+            while (i < interactionLength) {
+                var bot1Move = bot1.getNextMove(pastMoves1, payoffs, w)
+                ,   bot2Move = bot2.getNextMove(pastMoves2, payoffs, w)
+                ;
+                pastMoves1.push([bot1Move, bot2Move]);
+                pastMoves2.push([bot2Move, bot1Move]);
+                i++;
+            }
+            return pastMoves1;
+        }
+        /*  Make sure the inputs to runTournament make sense and if they do not,
+            say why in the list 'errors'
+
+            ARGS:
+            - botList: list of bots to participate in the tournament
+            - w: probability of interaction continuing at each step
+            - numMeetings: number of times each bot is paired with each
+            other bot
+            - payoffs: defines the scores for each Prisoner's Dilemma situation
+
+            RETURNS:
+            - errors: list or error messages to let the user know what is wrong
+            with the inputs, if anything
+            
+        */
+    ,   validateTournamentInputs:   function (botList, numMeetings, payoffs, w) {
+            var errors = [];
+            botList.forEach(function (bot) {
+                if (!bot instanceof global.BotPlayer)
+                    errors.push("botList must be a list of BotPlayer objects");
+            });
+            if (numMeetings < 1) errors.push("numMeetings must be at least 1");
+            if (!(payoffs.T > payoffs.R && payoffs.R > payoffs.P && payoffs.P > payoffs.S))
+                errors.push("payoffs must obey T > R > P > S");
+            if (2 * payoffs.R <= (payoffs.T + payoffs.S))
+                errors.push("payoffs must obey 2 * R > T + S");
+            if (w < 0 || w > 1) errors.append("w must be a number between 0 and 1");
+            return errors;
+        }
+        /*  Main method, partners each bot with each other bot with
+            w probability of ending each turn (length of interactions
+            is determined (using w) before any pairings, so all
+            pairings use the same list of interaction lengths)
+
+            ARGS:
+            - botList: list of bots to participate in the tournament
+            - w: probability of interaction continuing at each step
+            - numMeetings: number of times each bot is paired with each
+            other bot
+            - payoffs: defines the scores for each Prisoner's Dilemma situation
+
+            RETURNS:
+            - tourney_res: TournamentResults object with all the info
+            
+        */
+    ,   runTournament:  function (botList, numMeetings, payoffs, w) {
+            if (!payoffs) payoffs = { T: 5, R: 3, P: 1, S: 0 };
+            if (typeof w === "undefined") w = 0.995;
+            var errors = this.validateTournamentInputs(botList, numMeetings, payoffs, w);
+            if (errors.length) return global.error(errors);
+            var interactions = {}
+            ,   interactionLengths = this.generateInteractionLengths(w, numMeetings)
+            ;
+            for (var i = 0, n = botList.length; i < n; i++) botList[i].tournamentID = i;
+            var numBots = botList.length;
+            for (var i = 0, n = numBots; i < n; i++) {
+                for (var j = 0, o = numBots; j < o; j++) {
+                    var bot1 = botList[i]
+                    ,   bot2 = botList[j]
+                    ,   meetingResultsList = []
+                    ;
+                    for (var m = 0, p = numMeetings; m < p; m++) {
+                        meetingResultsList
+                            .push(this.botInteraction(bot1, bot2, interactionLengths[m], payoffs, w));
+                    }
+                    interactions[bot1.tournamentID + "-" + bot2.tournamentID] = meetingResultsList;
+                }
+            }
+            return new global.TournamentResults(botList, interactions, payoffs);
+        }
+    };
+    
+    
+    global.Arena = Arena;
+}(typeof exports !== "undefined" ? exports : window));
