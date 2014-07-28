6eddb11c73ca71c1c76dbcc7869f601572b1b57a
Robin Berjon <robin@berjon.com>
2014-07-28 12:53:02 +0200

    ported code for bot player base

---
+++ js/botplayer.js
@@ -0,0 +1,36 @@
+/*
+    BotPlayer represents a bot in an IPD tournament
+    Ported from https://github.com/tscizzle/IPD_Morality/
+***************************************************************************************************/
+
+/*
+    All bots are BotPlayers. They create an instance of BotPlayer to represent themselves.
+    The code here is different from the original Python. Instead of inheriting, bots just use this
+    to define themselves. They pass a callback that defines the behaviour instead of overriding.
+    
+    getNextMove
+    ARGS:
+    pastMoves: array of tuples, where each tuple is the pair
+        of choices made that turn by this bot and its partner
+        [(myMove1, hisMove1), (myMove2, hisMove2), ...] and
+        the moves are represented by 'C' for "Cooperate" or 'D'
+        for "Defect". For example, [('C', 'D'), ('D', 'D'), ...]
+
+    RETURNS:
+    nextMove: 'C' for "Cooperate" or 'D' for "Defect"
+*/
+
+(function (global) {
+    function BotPlayer (name, desc, nextMoveCB) {
+        this.name = name;
+        this.desc = desc || name;
+        this.tournamentID = null;
+        this.getNextMove = nextMoveCB.bind(this);
+    }
+    BotPlayer.prototype = {
+        toString:   function () {
+            return this.name;
+        }
+    };
+    global.BotPlayer = BotPlayer;
+}(typeof exports !== "undefined" ? exports : window));
